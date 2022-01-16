2292c0b8aea017c5175dc1d8a16eda8bfe4254fe
kanade <melty0404@gmail.com>
2022-01-16 17:39:15 +0900

    ch23

--- golang/xunit/xunit/main.py
+++ golang/xunit/xunit/main.py
@@ -1,12 +1,16 @@
 class TestResult:
     def __init__(self):
         self.runCount = 0
+        self.errorCount = 0
 
     def testStarted(self):
         self.runCount = self.runCount + 1
 
+    def testFailed(self):
+        self.errorCount = self.errorCount + 1
+
     def summary(self):
-        return "%d run, 0 failed" % self.runCount
+        return "%d run, %d failed" % (self.runCount, self.errorCount)
 
 
 class TestCase:
@@ -19,31 +23,55 @@ class TestCase:
     def tearDown(self):
         pass
 
-    def run(self):
+    def run(self, result: TestResult):
+        result.testStarted()
         self.setUp()
-        method = getattr(self, self.name)
-        method()
+        try:
+            method = getattr(self, self.name)
+            method()
+        except:
+            result.testFailed()
         self.tearDown()
 
 
 class TestCaseTest(TestCase):
     def setUp(self):
-        self.test = WasRun("testMethod")
+        self.result = TestResult()
 
     def testTemplateMethod(self):
         test = WasRun("testMethod")
-        test.run()
+        test.run(self.result)
         assert "setUp testMethod tearDown " == test.log
+
+    def testFailedFormatting(self):
+        self.result.testStarted()
+        self.result.testFailed()
+        assert "1 run, 1 failed" == self.result.summary()
+
+    def testSuite(self):
+        suite = TestSuite()
+        suite.add(WasRun("testMethod"))
+        suite.add(WasRun("testBrokenMethod"))
+        suite.run(self.result)
+        assert "2 run 1 failed" == self.result.summary()
+
+    def testResult(self):
+        test = WasRun("testMethod")
+        test.run(self.result)
+        assert "1 run, 0 failed" == self.result.summary()
+
+    def testFailResult(self):
+        test = WasRun("testBrokenMethod")
+        test.run(self.result)
+        assert "1 run, 1 failed" == self.result.summary()
 
 
 class WasRun(TestCase):
     def __init__(self, name: str):
         super().__init__(name)
         self.log = None
-        self.wasSetUp = None
 
     def setUp(self):
-        self.wasSetUp = 1
         self.log = "setUp "
 
     def testMethod(self):
@@ -56,6 +84,24 @@ class WasRun(TestCase):
         raise Exception
 
 
-TestCaseTest("testTemplateMethod").run()
-TestCaseTest("testResult").run()
-# TestCaseTest("testFailedResult").run()
+class TestSuite:
+    def __init__(self):
+        self.tests = []
+
+    def add(self, test):
+        self.tests.append(test)
+
+    def run(self, result: TestResult):
+        for test in self.tests:
+            test.run(result)
+
+
+suite = TestSuite()
+suite.add(TestCaseTest("testTemplateMethod"))
+suite.add(TestCaseTest("testResult"))
+suite.add(TestCaseTest("testFailedResult"))
+suite.add(TestCaseTest("testFailedResultFormatting"))
+suite.add(TestCaseTest("testSuite"))
+result = TestResult()
+suite.run(result)
+print(result.summary())
