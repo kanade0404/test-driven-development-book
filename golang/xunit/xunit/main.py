c129d33f9ff32364e492d54208f1f55bbc907476
kanade <melty0404@gmail.com>
2022-01-16 16:38:10 +0900

    ch19

--- golang/xunit/xunit/main.py
+++ golang/xunit/xunit/main.py
@@ -2,25 +2,41 @@ class TestCase:
     def __init__(self, name: str):
         self.name: str = name
 
+    def setUp(self):
+        pass
+
     def run(self):
+        self.setUp()
         method = getattr(self, self.name)
         method()
 
 
 class TestCaseTest(TestCase):
+    def setUp(self):
+        self.test = WasRun("testMethod")
+
     def testRunning(self):
-        test = WasRun("testMethod")
-        assert (not test.wasRun)
-        test.run()
-        assert (test.wasRun)
+        self.test.run()
+        assert self.test.wasRun
+
+    def testSetUp(self):
+        self.test.run()
+        assert self.test.wasSetUp
+
 
 class WasRun(TestCase):
-    def __init__(self, name: str) -> None:
+    def __init__(self, name: str):
+        super().__init__(name)
+        self.wasSetUp = None
         self.wasRun = None
-        super().__init__(name=name)
+
+    def setUp(self):
+        self.wasRun = None
+        self.wasSetUp = 1
 
     def testMethod(self):
         self.wasRun = 1
 
 
 TestCaseTest("testRunning").run()
+TestCaseTest("testSetUp").run()
